9f792589f83f79ea9aeccb99bf22fb744f327ede
Alexey <evil2142@bk.ru>
2020-11-19 16:43:07 +0300

    Implementation of the basic interface and logic

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let emojiCollection = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼",
+                           "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "👾"]
+    
+    var touches = 0 {
+        didSet {
+            touchersLabel.text = "Touchers: \(touches)"
+        }
     }
 
-
+    @IBOutlet weak var touchersLabel: UILabel!
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        let buttonIndex = buttonCollection.firstIndex(of: sender)
+        touches += 1
+        if let buttonIndex = buttonIndex {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.004997697193, green: 0.5134931207, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
