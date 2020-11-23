a573238218bfa9db1fc3c149fcc79d748ae4de26
Alexey <evil2142@bk.ru>
2020-11-23 10:12:01 +0300

    Implemented MVC. The model of the map. Game logic and data processing on the fronts.

---
+++ Concentration/Controller/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Alexey Ananin on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var game = ConcentrationGame(numberOfPairsOfCards: (buttonCollection.count + 1) / 2)
+    
+    var touches = 0 {
+        didSet {
+            touchersLabel.text = "Touchers: \(touches)"
+        }
+    }
+    
+    var emojiCollection = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼",
+                           "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "👾"]
+    
+    var emojiDictionary = [Int : String]()
+    func emojiIdentifier(for card: Card) -> String {
+        if emojiDictionary[card.identifier] == nil {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiCollection.count)))
+            emojiDictionary[card.identifier] = emojiCollection.remove(at: randomIndex)
+        }
+        return emojiDictionary[card.identifier] ?? "?"
+    }
+    
+    func updateViewFromModel() {
+        for index in buttonCollection.indices {
+            let button = buttonCollection[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emojiIdentifier(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                button.isEnabled = false
+            } else {
+                button.setTitle("", for: .normal)
+//                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0, green: 0.4877254367, blue: 1, alpha: 1)
+                if card.isMatched {
+                    button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+                    button.isEnabled = false
+                } else {
+                    button.backgroundColor = #colorLiteral(red: 0, green: 0.4860507846, blue: 0.9981886744, alpha: 1)
+                    button.isEnabled = true
+                }
+            }
+        }
+    }
+
+    @IBOutlet weak var touchersLabel: UILabel!
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            game.choseCard(at: buttonIndex)
+            updateViewFromModel()
+        }
+    }
+}
+
---
+++ Concentration/Model/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Alexey Ananin on 19.11.2020.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierNumber = 0
+    
+    static func identifierGenerator() -> Int {
+        identifierNumber += 1
+        return identifierNumber
+    }
+
+    init() {
+        self.identifier = Card.identifierGenerator()
+    }
+}
